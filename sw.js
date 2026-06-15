35b94c63ed5f94a3da9862491b2540cc7bd6dce8
Roberto Quiroz <quirozroberto293@gmail.com>
2026-06-15 00:57:11 -0600

    first commt

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "abog-karla-sierra-v1";
+const APP_ASSETS = [
+  "./",
+  "./plan-academico.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/icon-maskable-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match("./plan-academico.html"))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) =>
+      cachedResponse || fetch(event.request)
+    )
+  );
+});
